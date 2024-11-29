0a2b56022fb896c125f9cc397549d80e5b1dfff9
im23b-mahfuzr <mahfuzr@bzz.ch>
2024-11-29 22:48:26 +0100

    send

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const registerB = document.getElementById('registerB')
 const loginB = document.getElementById('loginB')
+const login = document.getElementById('login')
+const register = document.getElementById('register')
 const container= document.getElementById('container')
 const loginContainer = document.getElementById('login-container')
 
@@ -11,7 +13,21 @@ registerB.addEventListener('click', function clickRegister(){
         loginContainer.style.transition = "2s ease-in-out"
 });
 
+register.addEventListener('click', function clickRegister(){
+    container.style.left = "250px"
+    loginContainer.style.right = "-650px"
+    container.style.transition = "2s ease-in-out"
+    loginContainer.style.transition = "2s ease-in-out"
+});
+
 loginB.addEventListener('click', function clickLogin() {
+    loginContainer.style.right = "250px"
+    container.style.left = "-650px"
+    container.style.transition = "2s ease-in-out"
+    loginContainer.style.transition = "2s ease-in-out"
+});
+
+login.addEventListener('click', function clickLogin() {
     loginContainer.style.right = "250px"
     container.style.left = "-650px"
     container.style.transition = "2s ease-in-out"
