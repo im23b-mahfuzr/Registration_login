c539271095d8daaff18eaea687eaf69259f32fc1
im23b-mahfuzr <mahfuzr@bzz.ch>
2024-11-29 20:54:20 +0100

    send

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const registerB = document.getElementById('registerB')
 const loginB = document.getElementById('loginB')
+const container= document.getElementsByClassName('container')
+const loginContainer = document.getElementsByClassName('login-container')
+
+loginB.addEventListener("click" , function clickLogin() {
+    loginContainer.style.display = "block"
+    container.style.display = "none"
+});
+
+registerB.addEventListener("click" , function clickRegister() {
+    loginContainer.style.display = "none"
+    container.style.display = "block"
+});
